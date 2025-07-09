8706266035d2181ca6cf245a66a0942a73518f40
Parth-Chavda <parth18chavda@gmail.com>
2025-07-09 22:14:49 +0530

    content updated

--- pages/Template pages/blog.php
+++ pages/Template pages/blog.php
@@ -23,7 +23,7 @@ include pathof('include/nav.php');
             <div class="container py-5">
                 <div class="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 800px;">
                     <h1 class="display-5 text-capitalize mb-3">Cental<span class="text-primary"> Blog & News</span></h1>
-                    <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut amet nemo expedita asperiores commodi accusantium at cum harum, excepturi, quia tempora cupiditate! Adipisci facilis modi quisquam quia distinctio,
+                    <p class="mb-0">Stay updated with the latest car rental tips, travel trends, and news from the road. Our blog keeps you informed and inspired for every journey.
                     </p>
                 </div>
                 <div class="row g-4">
@@ -39,7 +39,7 @@ include pathof('include/nav.php');
                                     <div class="small"><span class="fa fa-comment-alt text-primary"></span><span class="ms-2">6 Comments</span></div>
                                 </div>
                                 <a href="#" class="h4 d-block mb-3">Rental Cars how to check driving fines?</a>
-                                <p class="mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius libero soluta impedit eligendi? Quibusdam, laudantium.</p>
+                                <p class="mb-3">Discover how to verify any pending traffic fines before renting a car to avoid surprises during your trip.</p>
                                 <a href="#" class="">Read More  <i class="fa fa-arrow-right"></i></a>
                             </div>
                         </div>
@@ -55,8 +55,8 @@ include pathof('include/nav.php');
                                     <div class="small"><span class="fa fa-user text-primary"></span><span class="ms-2">Martin.C</span></div>
                                     <div class="small"><span class="fa fa-comment-alt text-primary"></span><span class="ms-2">6 Comments</span></div>
                                 </div>
-                                <a href="#" class="h4 d-block mb-3">Rental cost of sport and other cars</a>
-                                <p class="mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius libero soluta impedit eligendi? Quibusdam, laudantium.</p>
+                                <a href="#" class="h4 d-block mb-3">Rental Costs: Sports vs. Standard Cars</a>
+                                <p class="mb-3">Curious about the price difference between luxury sports cars and everyday vehicles? Here’s what you need to know.</p>
                                 <a href="#" class="">Read More  <i class="fa fa-arrow-right"></i></a>
                             </div>
                         </div>
@@ -73,7 +73,7 @@ include pathof('include/nav.php');
                                     <div class="small"><span class="fa fa-comment-alt text-primary"></span><span class="ms-2">6 Comments</span></div>
                                 </div>
                                 <a href="#" class="h4 d-block mb-3">Document required for car rental</a>
-                                <p class="mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius libero soluta impedit eligendi? Quibusdam, laudantium.</p>
+                                <p class="mb-3">Before hitting the road, make sure you have the right documents. This guide breaks down exactly what you’ll need.</p>
                                 <a href="#" class="">Read More  <i class="fa fa-arrow-right"></i></a>
                             </div>
                         </div>
